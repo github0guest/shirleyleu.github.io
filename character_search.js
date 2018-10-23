4f9e94a4377dc81b666327b699eda40efdedf6e2
Shirley Leu <shirley.leu@schibsted.com>
2018-10-23 14:36:57 +0200

    Give the vue.js code a go around the block, as it were

---
+++ character_search.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '#search',
+    data: {
+        info: null,
+        errored: false,
+        searchTerm: '',
+    },
+    methods: {
+        search(term) {
+            this.newSearchTerm = term;
+            axios.post('http://gregoria.pythonanywhere.com/json/search/', JSON.stringify({text: this.searchTerm}))
+                .then(response => (this.info = response.data))
+                .catch(error => {
+                    console.log(error);
+                    this.errored = true
+                })
+        },
+        renderComic(date) {
+            //TODO: Dynamically rendering arbitrary HTML on your website can be very dangerous because it can easily
+            // lead to XSS vulnerabilities. Only use HTML interpolation on trusted content and never on user-provided content.
+            return date + `<img src="https://shirleyleu.github.io/static/images/` + date + `.gif">`
+        }
+    },
+});
