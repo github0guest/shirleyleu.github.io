8c445470d807e7fcbbb3763d16333021d9575890
Shirley Leu <shirley.leu@schibsted.com>
2018-11-02 14:50:17 +0100

    Add Next and Previous buttons

---
+++ character_search_copy.js
@@ -0,0 +1,32 @@
+new Vue({
+    el: '#search',
+    data: {
+        info: {},
+        found: false,
+        errored: false,
+        searchTerm: '',
+        base_URL: 'http://127.0.0.1:5000',
+        current_page: 1,
+    },
+    methods: {
+        renderComic(date) {
+            //TODO: Dynamically rendering arbitrary HTML on your website can be very dangerous because it can easily
+            // lead to XSS vulnerabilities. Only use HTML interpolation on trusted content and never on user-provided content.
+            return date.display_name + `<br /><img src="` + this.base_URL + `/static/images/` + date.filename + `.gif">`
+        },
+        search(term, target_page) {
+            this.found  = true;
+            axios.post(
+                this.base_URL+ '/json/search/',
+                JSON.stringify({text: this.searchTerm, current_page: target_page}))
+                .then(response => (this.info = response.data))
+                .catch(error => {
+                    console.log(error);
+                    this.errored = true
+                });
+            // if(this.info !== []){
+            //     this.found = true;
+            // }
+        },
+    },
+});
